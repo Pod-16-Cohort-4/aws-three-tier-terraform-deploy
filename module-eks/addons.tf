b4bb8e686d04af780a36990bb41b1508bc2c0760
pod16_cohort4 <pod16.cohort4@gmail.com>
2025-12-14 23:23:24 +0100

    addedon modified

--- module-eks/addons.tf
+++ module-eks/addons.tf
@@ -1,5 +1,6 @@
+
 provider "helm" {
-    kubernetes {
+    kubernetes = {
         host                   = aws_eks_cluster.eks.endpoint
         cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
         token                  = data.aws_eks_cluster_auth.eks.token
@@ -16,16 +17,41 @@ provider "kubernetes" {
 data "aws_eks_cluster_auth" "eks" {
     name = aws_eks_cluster.eks.name
 }
+
+# Create namespaces
+resource "kubernetes_namespace_v1" "ingress" {
+  provider = kubernetes.eks
+  metadata {
+    name = "ingress-nginx"
+  }
+}
+
+resource "kubernetes_namespace_v1" "cert_manager" {
+  provider = kubernetes.eks
+  metadata {
+    name = "cert-manager"
+  }
+}
+
+resource "kubernetes_namespace_v1" "argocd" {
+  provider = kubernetes.eks
+  metadata {
+    name = "argocd"
+  }
+}
+
+
 resource "helm_release" "nginx_ingress" {
     name       = "nginx-ingress"
     repository = "https://kubernetes.github.io/ingress-nginx"
     chart      = "ingress-nginx"
     version    = "4.12.0"
     namespace  = "ingress-nginx"
-    create_namespace = true
+    create_namespace = false
+    timeout = 600 
 
     values = [file("${path.module}/nginx-ingress-values.yaml")]
-    depends_on = [ aws_eks_node_group.eks_node_group ]
+    depends_on = [ aws_eks_node_group.eks_node_group, kubernetes_namespace_v1.ingress ]
 }
 
 data "aws_lb" "nginx_ingress" {
@@ -42,12 +68,23 @@ resource "helm_release" "cert_manager" {
     chart      = "cert-manager"
     version    = "1.14.5"
     namespace  = "cert-manager"
-    create_namespace = true
-    set {
-        name  = "installCRDs"
-        value = "true"
-    }
-    depends_on = [ helm_release.nginx_ingress ]
+    create_namespace = false
+    timeout = 600
+    wait = true
+    wait_for_jobs = true
+    
+    set = [
+        {
+            name  = "installCRDs"
+            value = "true"
+        },
+        {
+            name  = "global.leaderElection.namespace"
+            value = "cert-manager"
+        }
+    ]
+    
+    depends_on = [ helm_release.nginx_ingress, kubernetes_namespace_v1.cert_manager ]
 }
 #==================================================
 
@@ -57,7 +94,9 @@ resource "helm_release" "argocd" {
     chart            = "argo-cd"
     version          = "5.51.6"
     namespace        = "argocd"
-    create_namespace = true
+    create_namespace = false
+    wait = true
+    timeout = 600
     values = [file("${path.module}/argocd-values.yaml")]
-    depends_on = [ helm_release.nginx_ingress, helm_release.cert_manager]
+    depends_on = [ helm_release.nginx_ingress, helm_release.cert_manager, kubernetes_namespace_v1.argocd ]
 }
